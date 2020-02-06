1edbbe665e0a778de36215abecc3088bb6468b58
rajan <rajan.nit.cs@gmail.com>
2020-02-06 15:04:13 +0530

    Add model in matrix

--- matrixApp/admin.py
+++ matrixApp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from matrixApp.models import Post
 
-# Register your models here.
+
+#-------------------Register admin --------------------------
+class PostAdmin(admin.ModelAdmin):
+    list_display=['id','title','slug','author','body','publish','created','updated','status']
+    list_filter=['status','author','created']
+    search_fields=('title','body')
+    prepopulated_fields={'slug':('title',)}
+    raw_id_fields=('author',)
+    ordering=['status','publish']
+    date_hierarchy='publish'
+    
+#----------xx---------Register admin --------------x------------
+
+
+admin.site.register(Post,PostAdmin)
--- matrixApp/views.py
+++ matrixApp/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-
+from matrixApp.models import Post
 # Create your views here.
+
+
+#------------------Index page view -----------------------
 def index(request):
     return render(request,'HtmlFile/index.html')
+
+#--------x----------Index page view -----------x------------
+
+
+#-----------------Post list view-----------------------------
+def post_list_view(request):
+    post_list=Post.objects.all()
+    return render(request,'blog/post_list.html',{'post_list':post_list})
+#--------x---------Post list view----------------x-------------
